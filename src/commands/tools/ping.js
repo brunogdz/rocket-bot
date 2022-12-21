f552044c1879901eb25a2ee5abd00e5c4b9a5244
Bruno Gomes <brunog.vix@gmail.com>
2022-12-21 10:05:11 -0300

    first commit

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Returns the bot's ping"),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+      fetchReply: true,
+    });
+
+    const newMessage = `API Latency: ${client.ws.ping}ms\nClient Ping: ${
+      message.createdTimestamp - interaction.createdTimestamp
+    }ms`;
+    await interaction.editReply({
+      content: newMessage,
+    });
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const { token } = process.env;
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  PermissionsBitField,
+  Permissions,
+  Collection,
+} = require("discord.js");
+const fs = require("fs");
+
+const prefix = ">";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// client.on("ready", () => {
+//   console.log("Bot is online");
+
+//   client.user.setActivity(`Testing bot`, { type: "WATCHING" });
+// });
+
+// client.on("messageCreate", (message) => {
+//   if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+//   const args = message.content.slice(prefix.length).split(/ +/);
+//   // const argument = message.content.slice(prefix.length).split(/ +/)
+
+//   const command = args.shift().toLowerCase();
+
+//   // message array
+
+//   const messageArray = message.content.split(" ");
+//   const argument = messageArray.slice(1);
+//   const cmd = messageArray[0];
+
+//   if (command === "test") {
+//     message.channel.send("Bot is working");
+//   }
+// });
+
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
+
+client.handleEvents();
+client.handleCommands();
+
+client.login(token);
